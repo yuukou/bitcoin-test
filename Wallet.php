4d84c43f843dac25d9b69676c487a58cbedf7aa2
yuukou <yuukou.triplejump0219@gmail.com>
2020-09-15 22:21:42 +0900

    ビットコインアドレス生成処理を追加

--- Wallet.php
+++ Wallet.php
@@ -8,15 +8,31 @@ class Wallet
 {
     private string $privateKey;
     private string $publicKey;
+    private BitcoinECDSA $bitcoinECDSA;
 
+    /**
+     * Wallet constructor.
+     *
+     * @throws Exception
+     */
     public function __construct()
     {
-        $bitcoinECDSA = new BitcoinECDSA();
-        $bitcoinECDSA->generateRandomPrivateKey();
-        $this->privateKey = $bitcoinECDSA->getPrivateKey();
-        $this->publicKey = $bitcoinECDSA->getPubKey();
+        $this->bitcoinECDSA = new BitcoinECDSA();
+        $this->bitcoinECDSA->generateRandomPrivateKey();
+        $this->privateKey = $this->bitcoinECDSA->getPrivateKey();
+        $this->publicKey = $this->bitcoinECDSA->getPubKey();
+    }
+
+    /**
+     * ブロックチェーンアドレスを作成
+     *
+     * @return String
+     */
+    public function generateBlockChainAddress()
+    {
+        return $this->bitcoinECDSA->getAddress();
     }
 }
 
-//$wallet = new Wallet();
-//var_dump($wallet);
+$wallet = new Wallet();
+var_dump($wallet->generateBlockChainAddress());
